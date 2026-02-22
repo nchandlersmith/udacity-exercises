99bc700a7c7e69517ad1cc6a77a11543497e0184
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2026-02-22 10:13:48 -0600

    init commit

---
+++ config_manager.py
@@ -0,0 +1,11 @@
+"""
+ConfigManager module for managing application configuration.
+"""
+
+
+class ConfigManager:
+    """
+    A ConfigManager class that implements the Singleton pattern.
+    Ensures only one instance of the configuration manager exists.
+    """
+    
---
+++ test_config_manager.py
@@ -0,0 +1,13 @@
+"""
+Test module for ConfigManager class.
+"""
+
+import pytest
+from config_manager import ConfigManager
+
+
+class TestConfigManager:
+    """
+    Test class for ConfigManager functionality.
+    """
+    
