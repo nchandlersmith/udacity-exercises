62e66e45c0a94f738c82c6c1adf3bd692b6e592d
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2026-02-25 02:42:48 -0600

    feat: enhance StockMarket functionality with subscription management and update tests

--- behavioral_design_patterns/observer/stock_market.py
+++ behavioral_design_patterns/observer/stock_market.py
@@ -2,10 +2,23 @@ class Investor:
     def __init__(self, name):
         self._name = name
         self._notifications = []
-    
+
     @property
     def notifications(self):
         return self._notifications
-    
+
     def update(self, price):
-        self._notifications.append(f"{self._name} notified: stock price is now {price}")
+        self._notifications.append(
+            f"{self._name} notified: stock price is now {price}")
+
+
+class StockMarket():
+    def __init__(self):
+        self._subscribers = []
+
+    def subscribe(self, investor: Investor):
+        self._subscribers.append(investor)
+
+    @property
+    def subscribers(self):
+        return self._subscribers
--- behavioral_design_patterns/observer/test_stock_market.py
+++ behavioral_design_patterns/observer/test_stock_market.py
@@ -1,16 +1,23 @@
 """Test the stock market"""
 
-from stock_market import Investor
+from stock_market import Investor, StockMarket
 
 
 class TestStockMarket:
     def test_investor_should_update_price(self):
         investor = Investor("Some investor")
         investor.update(247.16)
-        
+
         result = investor.notifications
-        
+
         assert len(result) == 1
         assert result[0] == "Some investor notified: stock price is now 247.16"
+
+    def test_stock_market_add_investor(self):
+        market = StockMarket()
+        investor = Investor("Some investor")
         
-    
+        market.subscribe(investor)
+        
+        assert len(market.subscribers) == 1
+        assert market.subscribers[0] == investor
