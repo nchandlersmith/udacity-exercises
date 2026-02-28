326ab13913f507df743de908a46a44bc406e5c84
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2026-02-27 18:06:02 -0600

    feat: add PayPalPayment class and update payment messages for consistency

--- dependency_injection/checkout_service.py
+++ dependency_injection/checkout_service.py
@@ -9,7 +9,12 @@ class PaymentStrategy(ABC):
 
 class CreditCardPayment(PaymentStrategy):
     def pay(self, amount):
-        return f"Processing credit card payment of ${amount}"
+        return f"Processing credit card payment of ${amount}."
+
+
+class PayPalPayment(PaymentStrategy):
+    def pay(self, amount):
+        return f"Processing PayPal payment of ${amount}."
 
 
 class ShippingStrategy(ABC):
--- dependency_injection/test_checkout_service.py
+++ dependency_injection/test_checkout_service.py
@@ -1,8 +1,13 @@
-from checkout_service import CreditCardPayment
+from checkout_service import CreditCardPayment, PayPalPayment
 
 
 class TestCheckoutService():
     def test_credit_card_payment(self):
         card = CreditCardPayment()
         result = card.pay(491.55)
-        assert result == "Processing credit card payment of $491.55"
+        assert result == "Processing credit card payment of $491.55."
+
+    def test_paypal_payment(self):
+        card = PayPalPayment()
+        result = card.pay(67.42)
+        assert result == "Processing PayPal payment of $67.42."
