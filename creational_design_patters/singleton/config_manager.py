61bf690b10775efe58a358d53bb94add8a255531
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2026-03-01 07:39:27 -0600

    refactor: simplify instance creation in ConfigManager class

--- creational_design_patters/singleton/config_manager.py
+++ creational_design_patters/singleton/config_manager.py
@@ -12,7 +12,7 @@ class ConfigManager:
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(ConfigManager, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self):
